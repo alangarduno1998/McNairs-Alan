64eada11e045020ea9140ad837a245fc32dd9358
alangarduno1998 <agarduno1@islander.tamucc.edu>
2021-04-17 01:45:16 -0500

    added edge detection

--- venv/TrashDetector/trashthreshold.py
+++ venv/TrashDetector/trashthreshold.py
@@ -10,26 +10,34 @@ def thresholding(img):
     lower = np.array([hsvVals_red[0], hsvVals_red[1], hsvVals_red[2]])
     upper = np.array([hsvVals_red[3], hsvVals_red[4], hsvVals_red[5]])
     mask = cv2.inRange(hsv, lower, upper)
-    return mask
+    kernel = np.ones((2, 2), np.uint8)
+    dilation = cv2.dilate(img, kernel, iterations=1)
+
+    closing = cv2.morphologyEx(dilation, cv2.MORPH_GRADIENT, kernel)
+    closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+
+    # Getting the edge of morphology
+    edge = cv2.Canny(closing, 175, 175)
+    return mask, edge
 
 
 def getContours(imgThres, img):
     contours, hierarchy = cv2.findContours(imgThres, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # print(contours)
     biggest = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(biggest)
     cx = x + w // 2
     cy = y + h // 2
     cv2.drawContours(img, biggest, -1, (255,0,255), 7)
     cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
-    cnt = []
 
-    for i in list(range(len(contours))):
-        cnt.append([contours[i]])
-        x[i], y[i], w[i], h[i] = cv2.boundingRect(cnt[i])
-        cx[i] = x[i] + w[i] // 2
-        cy[i] = y[i] + h[i] // 2
-        cv2.drawContours(img, cnt[i], -1, (255, 0, 255), 7)
-        cv2.circle(img, (cx[i], cy[i]), 10, (0, 255, 0), cv2.FILLED)
+
+    cnt = contours[3]
+    x1, y1, w1, h1 = cv2.boundingRect(cnt)
+    cx1 = x1 + w1 // 2
+    cy1 = y1 + h1 // 2
+    cv2.drawContours(img, [cnt], 0, (255,0,255), 3)
+    cv2.circle(img, (cx1, cy1), 10, (0, 255, 0), cv2.FILLED)
 
     return cx
 
@@ -39,9 +47,10 @@ while True:
     img = cv2.resize(img, (width, height))
     img = cv2.flip(img, 0)
 
-    imgThres = thresholding(img)
+    imgThres, edge = thresholding(img)
 
-    cx, cx[i] = getContours(imgThres, img)  # for translation
+    cx = getContours(imgThres, img)  # for translation
     cv2.imshow("Output", img)
     cv2.imshow("Path", imgThres)
+    cv2.imshow("edges", edge)
     cv2.waitKey(1)
