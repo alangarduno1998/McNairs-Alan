0b4ed73a8ba2f16e571e241d708ed8e98f12dae0
alangarduno1998 <agarduno1@islander.tamucc.edu>
2021-04-18 13:54:29 -0500

    added functions to edge threshold and fixed display issues. included additional binary thresh to zero

--- venv/TrashDetector/edgethreshold.py
+++ venv/TrashDetector/edgethreshold.py
@@ -8,42 +8,38 @@ while(cap.isOpened()):
     #reading video to frames
     ret, frame = cap.read()
     frame = cv2.resize(frame, (width, height))
-    frame = cv2.flip(frame, 0)
+    #frame = cv2.flip(frame, 0)
     ret, display = cap.read()
     ret, image = cap.read()
     display = cv2.resize(display, (width, height))
-    display = cv2.flip(display, 0)
+    #display = cv2.flip(display, 0)
     image = cv2.resize(image, (width, height))
-    image = cv2.flip(image, 0)
-    #plotting contours to first frame
-    g = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    edge = cv2.Canny(g, 60, 180)
+    #image = cv2.flip(image, 0)
 
-    contours = cv2.findContours(edge,
-                                cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(frame, contours[0], -1, (0, 0, 255), thickness=2)
-    #plotting biggest contour to second frame
+    ret, mask = cap.read()
+    mask = cv2.resize(mask, (width, height))
+    #plotting contours to first frame
+
+    #applying binary threshold
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    r, t = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_TOZERO)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # -- applying canny edge detection
+    g = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    edge = cv2.Canny(g, 150, 150, L2gradient=True)
+    # plotting contours to display frame
     contours, h = cv2.findContours(edge,
                                    cv2.RETR_EXTERNAL,
                                    cv2.CHAIN_APPROX_NONE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
     cv2.drawContours(display, contours[0], -1, (0, 0, 255), thickness=5)
-
-    #plotting contours using binarization
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    r, t = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY_INV)
-    contours, h = cv2.findContours(t, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    cv2.drawContours(image, contours, -1, (0, 0, 255), thickness=5)
-
-    #displaying frames
-    #cv2.imshow("Output", frame)
-
+    result = cv2.bitwise_and(t, t, mask=edge)
     cv2.imshow("Output", np.hstack([frame,display,image]))
-    cv2.imshow('canny', edge)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('canny edges', edge)
+    cv2.imshow('binary threshold', t)
+    cv2.imshow('canny threshold', result)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
         break
 
 cap.release()
