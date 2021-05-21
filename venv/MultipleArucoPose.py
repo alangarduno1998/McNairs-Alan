52a4b6143c5b13a5e2ade6508d53658ca081edd9
alangarduno1998 <agarduno1@islander.tamucc.edu>
2021-05-20 21:49:35 -0500

    Improvements to PID and code

--- venv/MultipleArucoPose.py
+++ venv/MultipleArucoPose.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import os
+from time import time
 camera_matrix = np.float32([[1448, 0, 624], [0, 1448, 316], [0, 0, 1]])
 dist_coeff = np.float32([0.05437520427175414, 0.010684173729094198, 0.003107828628462368, -0.00950183296786585, 4.68352656147056])
 def findarucomarkers(frame, markersize = 4, totalmarkers=50, draw=True):
@@ -14,25 +15,28 @@ def findarucomarkers(frame, markersize = 4, totalmarkers=50, draw=True):
     if draw:
         display = aruco.drawDetectedMarkers(frame, corners, ids)
     return [corners, ids]
-def findaruco(cs, id, frame, AListC, AListA,AListR, AListT , drawPose=True):
+def findaruco(cs, id, frame, AListC, AListA,AListR, AListT ,start_time, drawPose=True):
     cx, cy = (cs[0][0][0] +cs[0][2][0]) / 2, (cs[0][0][1] + cs[0][3][1]) / 2
     area = (round(cv2.contourArea(cs) ** 0.5))
     rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(cs, 0.02, camera_matrix, dist_coeff)
     AListC.append((cx, cy)), AListR.append(rvec), AListT.append(tvec), AListA.append(area)
+    end_time = time()
+    dt = end_time - start_time
     if drawPose:
         aruco.drawAxis(frame, camera_matrix, dist_coeff, rvec, tvec, 0.1)
-    return frame, AListC, AListA, AListR, AListT
+    return frame, AListC, AListA, AListR, AListT, dt
 def main():
     cap = cv2.VideoCapture(0)
     AListA, AListC, AListR, AListT = [], [], [], []
     while True:
+        dt,start_time = 0, time()
         info = [[0, 0], [0], [0, 0, 0], [0, 0, 0]]
         ret, frame = cap.read()
         arucofound = findarucomarkers(frame)
         if len(arucofound[0]) != 0:
             for corners, id in zip(arucofound[0], arucofound[1]):
-                frame, info[0], info[1][0], info[2], info[3] = findaruco(corners, id, frame, AListC, AListA, AListR, AListT, drawPose=True)
-        print(info)
+                frame, info[0], info[1][0], info[2], info[3],dt = findaruco(corners, id, frame, AListC, AListA, AListR, AListT,start_time, drawPose=True)
+        print(dt)
         cv2.imshow('Display', frame)
         cv2.waitKey(1)
 if __name__ == "__main__":
